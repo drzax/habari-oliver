9a91608c8aadfc1cc24e05678daa8e581fd1db6b
Simon Elvery <simon@elvery.net>
2017-01-21 21:15:34 +1000

    http → https for google fonts

--- theme.php
+++ theme.php
@@ -5,7 +5,7 @@ class OliverTheme extends Theme
 	public function action_template_header($theme) 
 	{
 		Stack::add('template_stylesheet', $theme->get_url('/css/style.css'), 'style');
-		Stack::add('template_stylesheet', 'http://fonts.googleapis.com/css?family=Sorts+Mill+Goudy:400,400italic|PT+Sans:400');
+		Stack::add('template_stylesheet', 'https://fonts.googleapis.com/css?family=Sorts+Mill+Goudy:400,400italic|PT+Sans:400');
 	}
 	
 	/**
